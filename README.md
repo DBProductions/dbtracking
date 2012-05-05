8462949db176f4946f4bb74b462fd3a91c650dc9
DBProductions <HostmyCode4!>
2012-05-05 16:01:49 +0200

    add module for storage usage

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-Tracking with NodeJS
+Tracking with NodeJS and MongoDB or CouchDB
 
 require:
 - mongodb
+- cradle
+
+Write data to a local database the module can act with mongodb and couchdb.
+
+The data has JSON formatand looks like this:
+{url: params.url, site: params.site, timestamp: new Date().getTime()}
+
+How to track data:
+<img src="http://127.0.0.1:3000/?site=track_with_image_on_page&url=/">
+
+<script>
+var site = 'example site';
+var img = new Image();
+img.src = "http://127.0.0.1:3000/?site=" + site + "&url=" + document.location.href;
+</script>
--- tracking.js
+++ tracking.js
@@ -2,24 +2,31 @@ var sys = require('sys'),
     http = require('http'),
     querystring = require('querystring'),
     url = require('url'),
-    mongo = require('mongodb'),
-    db = new mongo.Db('tracking', new mongo.Server('127.0.0.1', 27017, {}), {});
+	modconn = require('./mod-conn');
+
+var options = {
+    db: 'mongodb',
+    host: 'localhost',
+	port: 27017,
+	dbname: 'tracking'
+}
+modconn.connection(options);    
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    db.open(function() {
-    	db.collection('pagetracks', function(err, collection){
-    		elems = url.parse(req.url);
-			params = querystring.parse(elems.query);
-			if (params.url === undefined || params.site === undefined) {
-				res.writeHead(200, {'Content-Type': 'image/jpeg'});
-				res.end();
-				return;
-			}
-			doc = {url: params.url, site: params.site, timestamp: new Date().getTime()};
-			collection.insert(doc, function() {
-				res.end();
-			});
-    	});
-    });
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/plain'});    
+    elems = url.parse(req.url);
+	params = querystring.parse(elems.query);
+	if (params.url === undefined || params.site === undefined) {
+		res.writeHead(200, {'Content-Type': 'image/gif'});
+		res.end();
+		return;
+	}
+	doc = {
+		url: params.url,
+		site: params.site,
+		timestamp: new Date().getTime()
+	};
+    modconn.save(doc);
+    res.end();
+}).listen(3000);
+sys.puts('Server runs and listen to http://127.0.0.1:3000/');
